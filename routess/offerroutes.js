4d9f8dd4c4a955d44cfeca78431c1bebc3091857
sanjay <san638233@gmail.com>
2025-07-05 12:19:18 +0530

    fsaf

--- routess/offerroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const offerController = require("../controllers/offercontroller.js");
-
-router.post("/", offerController.createOffer);
-router.get("/", offerController.getOffers);
-router.delete("/:id", offerController.deleteOffer);
-
-module.exports = router;
